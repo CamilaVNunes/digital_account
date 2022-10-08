6d29553e7e7ff4c6f23895da61a690f9707b6dd3
CamilaNunes <cvn.camila@outlook.com>
2022-10-08 17:54:20 -0300

    criando a camada application;

---
+++ DigitalAccount/DigitalAccount.Application/UseCases/AddCustomer/AddCustomerUseCase.cs
@@ -0,0 +1,26 @@
+﻿using DigitalAccount.Domain.Contracts.AddCustomer;
+using DigitalAccount.Domain.Entities;
+using DigitalAccount.Domain.UseCases.AddCustomer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalAccount.Application.UseCases.AddCustomer
+{
+    public class AddCustomerUseCase : IAddCustomerUseCase
+    {
+        private readonly IAddCustomerRepository _addCustomerRepository;
+
+        public AddCustomerUseCase(IAddCustomerRepository addCustomerRepository)
+        {
+            _addCustomerRepository = addCustomerRepository;
+        }
+
+        public void AddCustomer(Customer customer)
+        {
+            _addCustomerRepository.AddCustomer(customer);
+        }
+    }
+}
---
+++ DigitalAccount/DigitalAccount.Domain/Contracts/AddCustomer/IAddCustomerRepository.cs
@@ -0,0 +1,14 @@
+﻿using DigitalAccount.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalAccount.Domain.Contracts.AddCustomer
+{
+    public interface IAddCustomerRepository
+    {
+        void AddCustomer(Customer customer);
+    }
+}
