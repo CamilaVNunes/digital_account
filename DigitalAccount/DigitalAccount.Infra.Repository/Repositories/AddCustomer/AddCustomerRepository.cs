6f4244d389d0a6f5f54a45693cdae79f882faf4a
CamilaNunes <cvn.camila@outlook.com>
2022-10-08 18:02:53 -0300

    criando e configurando o Respository;

---
+++ DigitalAccount/DigitalAccount.Infra.Repository/Repositories/AddCustomer/AddCustomerRepository.cs
@@ -0,0 +1,20 @@
+﻿using DigitalAccount.Domain.Contracts.AddCustomer;
+using DigitalAccount.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalAccount.Infra.Repository.Repositories.AddCustomer
+{
+    public class AddCustomerRepository : IAddCustomerRepository
+    {
+        private readonly IList<Customer> _customerList = new List<Customer>();
+
+        public void AddCustomer(Customer customer)
+        {
+            _customerList.Add(customer);
+        }
+    }
+}
